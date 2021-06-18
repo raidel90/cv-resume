9f04170b9bcee7e46b9a0a28b383433698dbb04d
rvalle <rvalle@antel.net.uy>
2021-06-18 13:00:07 -0300

    Updates

---
+++ precache-manifest.8b3457796cf9239620c287c9bac33fb5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5eacdf954ef6651e128e84f3c241a36",
+    "url": "/cv-resume/index.html"
+  },
+  {
+    "revision": "7c0bf3f02586396cc7b2",
+    "url": "/cv-resume/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "c7231555359624bcf8c6",
+    "url": "/cv-resume/static/css/main.8ddc72f5.chunk.css"
+  },
+  {
+    "revision": "7c0bf3f02586396cc7b2",
+    "url": "/cv-resume/static/js/2.63d69f4d.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/cv-resume/static/js/2.63d69f4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7231555359624bcf8c6",
+    "url": "/cv-resume/static/js/main.eb23cd6e.chunk.js"
+  },
+  {
+    "revision": "c31297957e1b1b5f8e78",
+    "url": "/cv-resume/static/js/runtime-main.ea901d12.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.609962d12f27138331aa4735d7facb0a.js"
+  "/cv-resume/precache-manifest.8b3457796cf9239620c287c9bac33fb5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cv-resume/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
